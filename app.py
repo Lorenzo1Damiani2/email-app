bb29d15f57d7c27dfa81266365d9e993f74fbe9b
Lorenzo Damiani <72091414+Lorenzo1Damiani2@users.noreply.github.com>
2024-07-25 14:00:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime
-from fastapi import FastAPI
+from flask import Flask, jsonify
 
 df = pd.read_csv('data_with_dates.csv')
 df["date"] = pd.to_datetime(df["date"])
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
+@app.route('/')
 def send_email():
-    # Extract the word of the day and its meaning
     today = datetime.today().date()
     word_of_the_day_row = df[df['date'].dt.date == today]
 
@@ -22,13 +21,11 @@ def send_email():
         word_of_the_day = None
         meaning = None
 
-    # Display the variables
     print(f"Word of the Day (Catalan): {word_of_the_day}")
     print(f"Meaning (English): {meaning}")
 
     email_content = f"Word of the Day: {word_of_the_day}\nMeaning: {meaning}"
 
-    # Email settings
     sender_email = "lorenzodamiani06@gmail.com"
     receiver_email = "damianilorenzo06@gmail.com"
     password = "xwsl fvio vzpu xcwn"
@@ -38,7 +35,6 @@ def send_email():
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
-    # Send the email
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
             server.login(sender_email, password)
@@ -47,8 +43,7 @@ def send_email():
     except Exception as e:
         print(f"Failed to send email: {e}")
 
-    return {"message": "Email sent successfully!"}
+    return jsonify({"message": "Email sent successfully!"})
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
